d2c8416d662846fad4515cc4a9951904581aeacd
Ayush Pandey <99388483+ayushk2s@users.noreply.github.com>
2025-07-17 08:41:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def trade(request: TradeRequest):
             mtoken=request.mtoken,
             htoken=request.htoken,
             symbol=request.symbol,
-            side=request.side,
+            side=side,
             order_type=request.order_type,
             vol=request.vol,
             leverage=request.leverage,
